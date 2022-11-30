629ca02e5dfc00cbef68cfeb03229d4d2497d686
osawa-koki <github@osawa.dev>
2022-11-30 23:28:33 +0900

    たいとるまで。

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,26 @@ import { RemoveClassedElements } from './Logic/RemoveClassedElements';
 import { Logger } from './Logger';
 const logger = new Logger();
 
+const BIN = 2;
+const OCT = 8;
+const DEC = 10;
+const HEX = 16;
+
 (() => {
   const fileButton = document.getElementById('fileButton');
-
-  const byteArray: number[] = [];
+  const cellContainer = document.getElementById('cellContainer');
 
   if (fileButton === null) {
     logger.Error("'fileButton' is null");
     return;
   }
+  if (cellContainer === null) {
+    logger.Error("'cellContainer' is null");
+    return;
+  }
+
+  const byteArray: number[] = [];
+  // let uint8Array: Uint8Array = new Uint8Array();
 
   fileButton.addEventListener("change", function() {
     const files = (this as HTMLInputElement).files;
@@ -34,10 +45,20 @@ const logger = new Logger();
         return;
       }
       const dataView = new DataView((arrayBuffer as ArrayBuffer));
+      // uint8Array = new Uint8Array((arrayBuffer as ArrayBuffer));
       for (let i = 0; i < dataView.byteLength; i++) {
         byteArray.push(dataView.getUint8(i));
       }
       logger.Info(`Loaded ${byteArray.length} bytes`);
+      // logger.Info(`Loaded ${uint8Array.length} bytes`);
+
+      for (let i = 0; i < byteArray.length; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.textContent = byteArray[i].toString(HEX).toUpperCase().padStart(2, "0");
+        cell.title = `Byte [index: ${i+1}] [value: ${byteArray[i]}]`;
+        cellContainer.appendChild(cell);
+      }
     };
     reader.readAsArrayBuffer(file);
   });
